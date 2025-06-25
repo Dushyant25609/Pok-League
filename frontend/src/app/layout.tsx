e547dbc64ff9ef74e398c43ac786a78d595824a2
Dushyant <117421735+Honey25609@users.noreply.github.com>
2025-06-26 02:24:25 +0530

    fixing the footer

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-image bg-cover bg-center h-screen bg-no-repeat flex flex-col justify-center">
+      <body className="bg-image bg-cover bg-center h-screen bg-no-repeat flex flex-col justify-between">
         <BackgroundOverlay />
         <Toaster />
         {children}
